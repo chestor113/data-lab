1db01164018807b20a280834dd978ef06968340a
evgeny <chestor113@gmail.com>
2026-09-11 01:09:49 +0700

    in progress kafka cluster

--- terraform/network.tf
+++ terraform/network.tf
@@ -37,41 +37,16 @@ resource "yandex_vpc_security_group" "lb01" {
 resource "yandex_vpc_security_group" "kfk" {
   name       = "kfk-sg"
   network_id = yandex_vpc_network.vpnctl.id
-  ingress {
-    protocol       = "TCP"
-    port           = 22
-    v4_cidr_blocks = var.backend_subnet_cidr
-  }
 
   ingress {
-    protocol       = "ICMP"
+    protocol       = "ANY"
     v4_cidr_blocks = var.backend_subnet_cidr
   }
-
 
   egress {
     protocol       = "ANY"
     v4_cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-  protocol       = "TCP"
-  port           = 2181
-  v4_cidr_blocks = var.backend_subnet_cidr
-}
-
-ingress {
-  protocol       = "TCP"
-  port           = 2888
-  v4_cidr_blocks = var.backend_subnet_cidr
-}
-
-ingress {
-  protocol       = "TCP"
-  port           = 3888
-  v4_cidr_blocks = var.backend_subnet_cidr
-}
-
 }
 
 resource "yandex_vpc_route_table" "backend_gateway" {
